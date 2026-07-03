dc0a5bf9761222a413c1adb8954a071bb6f68aa3
Md Shakib Rafid Shakib <shakibrafidofficial@gmail.com>
2026-07-03 16:00:15 +0000

    Build home page layout

---
+++ my-app/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <h2>Email</h2>
+            <form>Email</form>
+            <h2>Password</h2>
+            <form>Passowrd</form>
+        </div>
+       
+    )
+}
+
+export default Login;
