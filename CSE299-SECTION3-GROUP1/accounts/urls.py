c628c42d3fc2eeda5f3614a7a107753c051a6846
Rahathossaincoder <abumdrahat@gmail.com>
2026-08-08 01:50:05 +0600

    mostly complete

--- CSE299-SECTION3-GROUP1/accounts/urls.py
+++ CSE299-SECTION3-GROUP1/accounts/urls.py
@@ -1,7 +1,20 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet
+
+from .views import ProfileViewSet, RegisterView
+
 
 router = DefaultRouter()
-router.register(r'profiles', ProfileViewSet, basename='profile')
 
-urlpatterns = router.urls
+router.register(
+    r'profiles',
+    ProfileViewSet,
+    basename='profile'
+)
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+]
+
+urlpatterns += router.urls
--- CSE299-SECTION3-GROUP1/accounts/views.py
+++ CSE299-SECTION3-GROUP1/accounts/views.py
@@ -1,19 +1,29 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from backend.permissions import IsOwnerOrStaff
-from .models import Profile
-from .serializers import ProfileSerializer
+
+from .models import User, Profile
+from .serializers import (
+    ProfileSerializer,
+    RegisterSerializer,
+    UserSerializer
+)
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
 
 class ProfileViewSet(viewsets.ModelViewSet):
-    '''ModelViewSet: By using this, Django automatically 
-    creates 5 endpoints for you: listing profiles, viewing a 
-    single profile, creating one, updating one, and deleting one.'''
     queryset = Profile.objects.all()
-    '''queryset = Profile.objects.all(): This tells the view: 
-    "When you need to look at the database, use the Profile table."
-    '''
     serializer_class = ProfileSerializer
 
     def get_permissions(self):
         if self.action == 'create':
             return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated(), IsOwnerOrStaff()]
+
+        return [
+            permissions.IsAuthenticated(),
+            IsOwnerOrStaff()
+        ]
