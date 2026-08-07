0886beede7eddb8609a32c46896f2f8dcd7320f4
Rahathossaincoder <abumdrahat@gmail.com>
2026-08-08 01:56:45 +0600

    mostly complete-2

--- CSE299-SECTION3-GROUP1/my-app/vite.config.js
+++ CSE299-SECTION3-GROUP1/my-app/vite.config.js
@@ -4,9 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
     proxy: {
       '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+
+      '/media': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
